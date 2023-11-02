3c642816f6feced40623b29b94f71a16f031ec46
William David <william.david93000@gmail.com>
2023-11-02 07:24:39 +0100

    Apres deploiement azure prod erreur du 02 11 2023

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+az acr build --registry monappregistrywd --resource-group appgroup --image python_docker ./python_docker
+az acr build --registry monappregistrywd --resource-group appgroup --image dashboard_docker ./dashboard_docker
+az acr build --registry monappregistrywd --resource-group appgroup --image api_docker ./api_docker
